42b711db61626ddc00c306de95fe751db85b6c7d
Aleksei Kuznetcov <kuznetz.vrn@gmail.com>
2017-06-23 00:06:18 -0400

    Applied isort utility

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,6 +1,7 @@
-from rest_framework import serializers
-from .models import Task
 from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from .models import Task
 
 
 class TaskSerializer(serializers.HyperlinkedModelSerializer):
